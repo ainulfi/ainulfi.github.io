1b886baf54a5592b40321abb36eb22ea3f467f62
ainulfi <enulmaf@gmail.com>
2024-03-25 10:32:12 +0700

    first commit of project

---
+++ application/views/template/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+Template Name: NobleUI - Admin & Dashboard Template
+Author: NobleUI
+Website: https://www.nobleui.com
+Contact: nobleui123@gmail.com
+Purchase: https://1.envato.market/nobleui_admin
+License: You must have a valid license purchased only from above link or https://themeforest.net/user/nobleui/portfolio/ in order to legally use the theme for your project.
+-->
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Dashboard BBPPMPV BMTI</title>
+	<!-- core:css -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/vendors/core/core.css">
+	<!-- endinject -->
+  <!-- plugin css for this page -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
+	<!-- end plugin css for this page -->
+	<!-- inject:css -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/fonts/feather-font/css/iconfont.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/vendors/flag-icon-css/css/flag-icon.min.css">
+	<!-- endinject -->
+  <!-- Layout styles -->  
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/css/demo_5/style.css">
+  <!-- End layout styles -->
+  	<link rel="shortcut icon" href="<?= base_url(); ?>assets/images/favicon_bmti.png" />
+  <!-- JVectorMap Css -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/plugins/jvectormap/jquery-jvectormap-2.0.3.css"/>
+</head>
---
+++ application/views/template/topbar.php
@@ -0,0 +1,62 @@
+<body>
+	<div class="main-wrapper">
+
+		<!-- partial:partials/_navbar.html -->
+		<div class="horizontal-menu">
+			<nav class="navbar top-navbar">
+				<div class="container">
+					<div class="navbar-content">
+						<img src="<?= base_url(); ?>assets/images/bmti.png" style="width: 50px; height: 50px">
+						<a href="#" class="navbar-brand">
+							Informasi Data dan Statistik BBPPMPV BMTI
+						</a>
+					</div>
+				</div>
+			</nav>
+			<nav class="bottom-navbar">
+				<div class="container">
+					<ul class="nav page-navigation">
+						<li class="nav-item">
+							<a class="nav-link" href="<?= base_url(); ?>dashboard">
+								<i class="link-icon" data-feather="box"></i>
+								<span class="menu-title">Dashboard</span>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="link-icon" data-feather="mail"></i>
+								<span class="menu-title">Peningkatan Kompetensi</span>
+								<i class="link-arrow"></i>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="link-icon" data-feather="inbox"></i>
+								<span class="menu-title">Penjaminan Mutu</span>
+								<i class="link-arrow"></i>
+							</a>
+						</li>
+						<li class="nav-item mega-menu">
+							<a href="#" class="nav-link">
+								<i class="link-icon" data-feather="feather"></i>
+								<span class="menu-title">Kepuasan Masyarakat</span>
+								<i class="link-arrow"></i>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="link-icon" data-feather="pie-chart"></i>
+								<span class="menu-title">Data Pegawai</span>
+								<i class="link-arrow"></i>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<span class="menu-title"><b>Data & Statistik <br> BBPPMPV BMTI</b></span>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+		<!-- partial -->
